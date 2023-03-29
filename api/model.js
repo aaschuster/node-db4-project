081269d7a33a92aa5355eb332d45a2b6cb41a1c6
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-29 13:48:03 -0400

    build out basic database function and endpoint

--- api/model.js
+++ api/model.js
@@ -1,5 +1,12 @@
 const db = require("../data/dbconfig");
 
-function getRecipeById(recipe_id) {
-    db("recipes")
+async function getRecipeById(recipe_id) {
+    const rows = await db("recipes as r")
+    .join("steps as s", "r.recipe_id", "s.recipe_id")
+    .leftJoin("step_ingredients as si", "s.step_id", "si.step_ingredient_id")
+    .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id");
+
+    return rows;
 }
+
+module.exports = {getRecipeById};
--- api/server.js
+++ api/server.js
@@ -3,6 +3,12 @@ const server = express();
 
 server.use(express.json());
 
-//endpoints here
+const Recipes = require("./model");
+
+server.get("/api/recipes/:id", (req, res) => {
+    Recipes.getRecipeById(req.params.id)
+        .then( recipe => res.json(recipe))
+        .catch( () => res.status(500).json({message: "There was an issue with the server"}));
+})
 
 module.exports = server;
