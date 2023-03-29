6f6332840b2821550fc14b483a3b8ceab8c79606
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-29 13:49:41 -0400

    update db function to get recipe by id

--- api/model.js
+++ api/model.js
@@ -4,8 +4,8 @@ async function getRecipeById(recipe_id) {
     const rows = await db("recipes as r")
     .join("steps as s", "r.recipe_id", "s.recipe_id")
     .leftJoin("step_ingredients as si", "s.step_id", "si.step_ingredient_id")
-    .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id");
-
+    .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id")
+    .where("r.recipe_id", recipe_id);
     return rows;
 }
 
